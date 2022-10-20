6c4d61d163b2e9e0b42c18f1db7823875961fc1a
thi-araujo <th.araujo@outlook.com>
2022-10-19 22:59:29 -0300

    "Verifica se o script esta sendo executado como Administrador"

--- Menu_Start_v0.1.2r.bat
+++ Menu_Start_v0.1.2r.bat
@@ -15,5 +15,19 @@ chcp 1252 >nul 2>&1
 rem --> Verifica se o cmd esta sendo executado como Administrador
 net file >nul 2>&1
 
+rem --> Verifica se o script esta sendo executado como Administrador
+if '%errorlevel%' == '0' (
+    goto begin
+) else (
+    echo.
+    echo N�o � poss�vel executar este script corretamente se voc� n�o possuir privil�gios de Administrador
+    timeout /t 3 >nul 2>&1
+    cls
+    echo.
+    echo O script ser� encerrado, execute ele novamente como Administrador para prosseguir.
+    timeout /t 3 >nul 2>&1
+    goto eof
+    )
+
 
 
