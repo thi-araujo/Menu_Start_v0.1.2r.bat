4feb814d74e18b5274b1697da7b52e28f7e40749
thi-araujo <th.araujo@outlook.com>
2022-11-15 20:50:33 -0300

    "atualização de comentário"

--- Menu_Start_v0.1.2r.bat
+++ Menu_Start_v0.1.2r.bat
@@ -7,7 +7,7 @@ rem Data da cria��o: 18/09/2022
 rem Data da revis�o: 04/10/2022
 
 
-rem PS: Este script foi baseado no antigo script que havia criado para atualiza��o dos desktop.
+rem PS: Este script foi baseado no antigo script que havia criado para atualização dos desktop.
 
 rem --> Altera a pagina do cmd para 1252 deste modo posso trabalhar com acentos e caracteres especiais
 chcp 1252 >nul 2>&1
