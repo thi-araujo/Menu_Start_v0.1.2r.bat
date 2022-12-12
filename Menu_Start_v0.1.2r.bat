7d906ed3fbb95ea7925646ed370968524b715123
thi-araujo <th.araujo@outlook.com>
2022-12-12 15:53:30 -0300

    "menu de acções"

--- Menu_Start_v0.1.2r.bat
+++ Menu_Start_v0.1.2r.bat
@@ -45,7 +45,7 @@ del /q %tmp%\folderIcones.txt >nul 2>&1
 timeout /t 3 >nul 2>&1
 
 :startMenu
-rem --> Abaixo temos as opções do menu de ações
+rem --> Abaixo temos as opções do menu de ações:
 cls
 
 title Menu de ações
